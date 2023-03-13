2b0e42c9bb1a2972fdd4b1f269468970ceebdda4
Santhosh <santhoshkalish333@gmail.com>
2023-03-14 02:24:39 +0530

    created - basic calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,97 @@
 import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+function Calculator() {
+  const [input, setInput] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const NumberClick = (event) => {
+    setInput(input + event.target.value);
+  };
+
+  const OperationClick = (event) => {
+    setInput(input + event.target.value);
+  };
+
+  const ClearClick = () => {
+    setInput("");
+    setAnswer("");
+  };
+
+  const EqualsClick = () => {
+    try {
+      const result = eval(input);
+      setAnswer(result);
+    } catch (error) {
+      setAnswer("Error");
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <input type="text" value={input} />
+      <div className="buttons">
+        <button onClick={NumberClick} value="7">
+          7
+        </button>
+        <button onClick={NumberClick} value="8">
+          8
+        </button>
+        <button onClick={NumberClick} value="9">
+          9
+        </button>
+        <button onClick={OperationClick} value="+">
+          +
+        </button>
+
+        <br></br>
+
+        <button onClick={NumberClick} value="4">
+          4
+        </button>
+        <button onClick={NumberClick} value="5">
+          5
+        </button>
+        <button onClick={NumberClick} value="6">
+          6
+        </button>
+        <button onClick={OperationClick} value="-">
+          -
+        </button>
+
+        <br></br>
+
+        <button onClick={NumberClick} value="1">
+          1
+        </button>
+        <button onClick={NumberClick} value="2">
+          2
+        </button>
+        <button onClick={NumberClick} value="3">
+          3
+        </button>
+        <button onClick={OperationClick} value="*">
+          *
+        </button>
+
+        <br></br>
+
+        <button onClick={ClearClick} value="clear">
+          Clear
+        </button>
+        <button onClick={NumberClick} value="0">
+          0
+        </button>
+        <button onClick={EqualsClick} value="=">
+          =
+        </button>
+        <button onClick={OperationClick} value="/">
+          /
+        </button>
+      </div>
+      <div className="result">{answer}</div>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
